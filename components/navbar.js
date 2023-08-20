60e1839430bcf409e60e8c32fe1fd259678b0a96
Kinyanjui Karanja <ekkinyanjui@gmail.com>
2023-08-20 14:53:09 +0300

    Added index page

--- components/navbar.js
+++ components/navbar.js
@@ -9,6 +9,7 @@ import { urlForImage } from "@/lib/sanity/image";
 import cx from "clsx";
 import { ChevronDownIcon } from "@heroicons/react/24/solid";
 import { myLoader } from "@/utils/all";
+import { DarkThemeToggle, Flowbite } from "flowbite-react";
 
 export default function Navbar(props) {
   const leftmenu = [
@@ -21,6 +22,14 @@ export default function Navbar(props) {
       href: "/about"
     },
     {
+      label: "Portfolio",
+      href: "/portfolio"
+    },
+    {
+      label: "Blog",
+      href: "/blog"
+    },
+    {
       label: "Contact",
       href: "/contact"
     }
@@ -30,151 +39,126 @@ export default function Navbar(props) {
     {
       label: "Archive",
       href: "/archive"
-    },
+    }
   ];
 
   const mobilemenu = [...leftmenu, ...rightmenu];
 
   return (
-    <Container>
-      <nav>
-        <Disclosure>
-          {({ open }) => (
-            <>
-              <div className="flex flex-wrap justify-between md:flex-nowrap md:gap-10">
-                <div className="order-1 hidden w-full flex-col items-center justify-start md:order-none md:flex md:w-auto md:flex-1 md:flex-row md:justify-end">
-                  {leftmenu.map((item, index) => (
-                    <Fragment key={`${item.label}${index}`}>
-                      {item.children && item.children.length > 0 ? (
-                        <DropdownMenu
-                          menu={item}
-                          key={`${item.label}${index}`}
-                          items={item.children}
-                        />
-                      ) : (
-                        <Link
-                          href={item.href}
-                          key={`${item.label}${index}`}
-                          className="px-5 py-2 text-sm font-medium text-gray-600 hover:text-blue-500 dark:text-gray-400"
-                          target={item.external ? "_blank" : ""}
-                          rel={item.external ? "noopener" : ""}>
-                          {item.label}
-                        </Link>
-                      )}
-                    </Fragment>
-                  ))}
-                </div>
-                <div className="flex w-full items-center justify-between md:w-auto">
-                  <Link href="/" className="w-28 dark:hidden">
-                    {props.logo ? (
-                      <Image
-                        {...urlForImage(props.logo)}
-                        alt="Logo"
-                        priority={true}
-                        sizes="(max-width: 640px) 100vw, 200px"
-                      />
-                    ) : (
-                      <span className="block text-center">
-                        Stablo
-                      </span>
-                    )}
-                  </Link>
-                  <Link href="/" className="hidden w-28 dark:block">
-                    {props.logoalt ? (
-                      <Image
-                        {...urlForImage(props.logoalt)}
-                        alt="Logo"
-                        priority={true}
-                        sizes="(max-width: 640px) 100vw, 200px"
-                      />
-                    ) : (
-                      <span className="block text-center">
-                        Stablo
-                      </span>
-                    )}
-                  </Link>
-                  <Disclosure.Button
-                    aria-label="Toggle Menu"
-                    className="ml-auto rounded-md px-2 py-1 text-gray-500 focus:text-blue-500 focus:outline-none dark:text-gray-300 md:hidden ">
-                    <svg
-                      className="h-6 w-6 fill-current"
-                      xmlns="http://www.w3.org/2000/svg"
-                      viewBox="0 0 24 24">
-                      {open && (
-                        <path
-                          fillRule="evenodd"
-                          clipRule="evenodd"
-                          d="M18.278 16.864a1 1 0 0 1-1.414 1.414l-4.829-4.828-4.828 4.828a1 1 0 0 1-1.414-1.414l4.828-4.829-4.828-4.828a1 1 0 0 1 1.414-1.414l4.829 4.828 4.828-4.828a1 1 0 1 1 1.414 1.414l-4.828 4.829 4.828 4.828z"
-                        />
-                      )}
-                      {!open && (
-                        <path
-                          fillRule="evenodd"
-                          d="M4 5h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2z"
-                        />
-                      )}
-                    </svg>
-                  </Disclosure.Button>
-                </div>
+    <Flowbite>
+      <Container>
+        <nav>
+          <Disclosure>
+            {({ open }) => (
+              <>
+                <div className="flex flex-wrap justify-between md:flex-nowrap md:gap-10">
+                  <div className="order-1 hidden w-full flex-col items-center justify-start md:order-none md:flex md:w-auto md:flex-1 md:flex-row md:justify-end">
+                    {leftmenu.map((item, index) => (
+                      <Fragment key={`${item.label}${index}`}>
+                        {item.children && item.children.length > 0 ? (
+                          <DropdownMenu
+                            menu={item}
+                            key={`${item.label}${index}`}
+                            items={item.children}
+                          />
+                        ) : (
+                          <Link
+                            href={item.href}
+                            key={`${item.label}${index}`}
+                            className="px-5 py-2 text-sm font-medium text-gray-600 hover:text-blue-500 dark:text-gray-400"
+                            target={item.external ? "_blank" : ""}
+                            rel={item.external ? "noopener" : ""}>
+                            {item.label}
+                          </Link>
+                        )}
+                      </Fragment>
+                    ))}
+                  </div>
+                  <div className="flex w-full items-center justify-between md:w-auto">
+                    <Disclosure.Button
+                      aria-label="Toggle Menu"
+                      className="ml-auto rounded-md px-2 py-1 text-gray-500 focus:text-blue-500 focus:outline-none dark:text-gray-300 md:hidden ">
+                      <svg
+                        className="h-6 w-6 fill-current"
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 24 24">
+                        {open && (
+                          <path
+                            fillRule="evenodd"
+                            clipRule="evenodd"
+                            d="M18.278 16.864a1 1 0 0 1-1.414 1.414l-4.829-4.828-4.828 4.828a1 1 0 0 1-1.414-1.414l4.828-4.829-4.828-4.828a1 1 0 0 1 1.414-1.414l4.829 4.828 4.828-4.828a1 1 0 1 1 1.414 1.414l-4.828 4.829 4.828 4.828z"
+                          />
+                        )}
+                        {!open && (
+                          <path
+                            fillRule="evenodd"
+                            d="M4 5h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2z"
+                          />
+                        )}
+                      </svg>
+                    </Disclosure.Button>
+                  </div>
 
-                <div className="order-2 hidden w-full flex-col items-center justify-start md:order-none md:flex md:w-auto md:flex-1 md:flex-row">
-                  {rightmenu.map((item, index) => (
-                    <Fragment key={`${item.label}${index}`}>
-                      {item.children && item.children.length > 0 ? (
-                        <DropdownMenu
-                          menu={item}
-                          key={`${item.label}${index}`}
-                          items={item.children}
-                        />
-                      ) : (
-                        <Link
-                          href={item.href}
-                          key={`${item.label}${index}`}
-                          className="px-5 py-2 text-sm font-medium text-gray-600 hover:text-blue-500 dark:text-gray-400"
-                          target={item.external ? "_blank" : ""}
-                          rel={item.external ? "noopener" : ""}>
-                          <span> {item.label}</span>
-                          {item.badge && (
-                            <span className="ml-2 rounded bg-blue-100 px-2 py-0.5 text-xs font-semibold text-blue-600 dark:bg-cyan-200 dark:text-blue-800 ">
-                              {item.badge}
-                            </span>
-                          )}
-                        </Link>
-                      )}
-                    </Fragment>
-                  ))}
+                  <div className="order-2 hidden w-full flex-col items-center justify-start md:order-none md:flex md:w-auto md:flex-1 md:flex-row">
+                    {rightmenu.map((item, index) => (
+                      <Fragment key={`${item.label}${index}`}>
+                        {item.children && item.children.length > 0 ? (
+                          <DropdownMenu
+                            menu={item}
+                            key={`${item.label}${index}`}
+                            items={item.children}
+                          />
+                        ) : (
+                          <Link
+                            href={item.href}
+                            key={`${item.label}${index}`}
+                            className="px-5 py-2 text-sm font-medium text-gray-600 hover:text-blue-500 dark:text-gray-400"
+                            target={item.external ? "_blank" : ""}
+                            rel={item.external ? "noopener" : ""}>
+                            <span> {item.label}</span>
+                            {item.badge && (
+                              <span className="ml-2 rounded bg-blue-100 px-2 py-0.5 text-xs font-semibold text-blue-600 dark:bg-cyan-200 dark:text-blue-800 ">
+                                {item.badge}
+                              </span>
+                            )}
+                          </Link>
+                        )}
+                      </Fragment>
+                    ))}
+                    <DarkThemeToggle className="ml-auto" />
+                  </div>
                 </div>
-              </div>
-              <Disclosure.Panel>
-                <div className="order-2 -ml-4 mt-4 flex w-full flex-col items-center justify-start md:hidden">
-                  {mobilemenu.map((item, index) => (
-                    <Fragment key={`${item.label}${index}`}>
-                      {item.children && item.children.length > 0 ? (
-                        <DropdownMenu
-                          menu={item}
-                          key={`${item.label}${index}`}
-                          items={item.children}
-                          mobile={true}
-                        />
-                      ) : (
-                        <Link
-                          href={item.href}
-                          key={`${item.label}${index}`}
-                          className="w-full px-5 py-2 text-sm font-medium text-gray-600 hover:text-blue-500 dark:text-gray-400"
-                          target={item.external ? "_blank" : ""}
-                          rel={item.external ? "noopener" : ""}>
-                          {item.label}
-                        </Link>
-                      )}
-                    </Fragment>
-                  ))}
-                </div>
-              </Disclosure.Panel>
-            </>
-          )}
-        </Disclosure>
-      </nav>
-    </Container>
+                <Disclosure.Panel>
+                  <div className="order-2 -ml-4 mt-4 flex w-full flex-col items-center justify-start md:hidden">
+                    {mobilemenu.map((item, index) => (
+                      <Fragment key={`${item.label}${index}`}>
+                        {item.children && item.children.length > 0 ? (
+                          <DropdownMenu
+                            menu={item}
+                            key={`${item.label}${index}`}
+                            items={item.children}
+                            mobile={true}
+                          />
+                        ) : (
+                          <Link
+                            href={item.href}
+                            key={`${item.label}${index}`}
+                            className="w-full px-5 py-2 text-sm font-medium text-gray-600 hover:text-blue-500 dark:text-gray-400"
+                            target={item.external ? "_blank" : ""}
+                            rel={item.external ? "noopener" : ""}>
+                            {item.label}
+                          </Link>
+                        )}
+                      </Fragment>
+                    ))}
+                  </div>
+                </Disclosure.Panel>
+              </>
+            )}
+          </Disclosure>
+        </nav>
+      </Container>
+    </Flowbite>
   );
 }
 
