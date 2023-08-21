34676a73c84d7a0e7fd06461ef7b44e8f93d7b55
Kinyanjui Karanja <ekkinyanjui@gmail.com>
2023-08-21 18:18:48 +0300

    Changed Home Page

--- next.config.js
+++ next.config.js
@@ -14,6 +14,14 @@ const nextConfig = {
   eslint: {
     /// Set this to false if you want production builds to abort if there's lint errors
     ignoreDuringBuilds: process.env.VERCEL_ENV === "production"
+  },
+  async rewrites() {
+    return [
+      {
+        source: "/",
+        destination: "/index.html"
+      }
+    ];
   }
 };
 
